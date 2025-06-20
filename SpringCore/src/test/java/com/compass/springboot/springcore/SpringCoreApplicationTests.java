ce3c44bfe34cbde824232a6ac8d60731d0d44828
JVKszuba <jvkaszuba@gmail.com>
2025-06-20 10:56:13 -0300

    chore: add springcore project

---
+++ SpringCore/src/test/java/com/compass/springboot/springcore/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.springboot.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
