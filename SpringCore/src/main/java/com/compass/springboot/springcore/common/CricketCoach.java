5d4eeb46181bde8ab80bdf7d281bec4e4d0eb02c
JVKszuba <jvkaszuba@gmail.com>
2025-06-24 09:57:55 -0300

    feat: implement bean initialization and destruction hooks

--- SpringCore/src/main/java/com/compass/springboot/springcore/common/CricketCoach.java
+++ SpringCore/src/main/java/com/compass/springboot/springcore/common/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.compass.springboot.springcore.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,18 @@ public class CricketCoach implements Coach {
     public CricketCoach() {
 
         System.out.println("In constructor: " + this.getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void init() {
+
+        System.out.println("In postConstruct: " + this.getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void destroy() {
+
+        System.out.println("In preDestroy: " + this.getClass().getSimpleName());
     }
 
     @Override
