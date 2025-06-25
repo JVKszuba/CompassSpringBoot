c6af7767f12032b69f7199c8adaf25d89951fd4e
JVKszuba <jvkaszuba@gmail.com>
2025-06-25 10:15:03 -0300

    feat: add studentDAO, implementation and test data insertion

--- HibernateCRUD/src/main/java/com/compass/springboot/hibernatecrud/HibernateCrudApplication.java
+++ HibernateCRUD/src/main/java/com/compass/springboot/hibernatecrud/HibernateCrudApplication.java
@@ -1,7 +1,11 @@
 package com.compass.springboot.hibernatecrud;
 
+import com.compass.springboot.hibernatecrud.dao.StudentDAO;
+import com.compass.springboot.hibernatecrud.entity.Student;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HibernateCrudApplication {
@@ -10,4 +14,20 @@ public class HibernateCrudApplication {
         SpringApplication.run(HibernateCrudApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+
+        return runner -> createStudent(studentDAO);
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+
+        System.out.println("Creating new student object ...");
+        Student student = new Student("Paul", "Doe", "paul@gmail.com");
+
+        System.out.println("Saving student object ...");
+        studentDAO.save(student);
+
+        System.out.println("Saved student. Generated id: " + student.getId());
+    }
 }
---
+++ HibernateCRUD/src/main/java/com/compass/springboot/hibernatecrud/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.compass.springboot.hibernatecrud.dao;
+
+import com.compass.springboot.hibernatecrud.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+}
