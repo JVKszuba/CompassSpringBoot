4010699d4bb5ef8841f1afe732308b5284ce3941
JVKszuba <jvkaszuba@gmail.com>
2025-06-27 12:01:32 -0300

    feat: add controller and integrate with Thymeleaf template

---
+++ SpringMVC/src/main/java/com/compass/springboot/springmvc/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.compass.springboot.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+
+        model.addAttribute("time", java.time.LocalDateTime.now());
+
+        return "helloworld";
+    }
+}
