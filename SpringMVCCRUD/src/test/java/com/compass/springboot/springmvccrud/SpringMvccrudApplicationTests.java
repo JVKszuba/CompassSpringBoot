616a9b349957f2c5097b6532982f0c70943420d6
JVKszuba <jvkaszuba@gmail.com>
2025-06-30 15:28:39 -0300

    chore: add spring MVC CRUD project

---
+++ SpringMVCCRUD/src/test/java/com/compass/springboot/springmvccrud/SpringMvccrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.springboot.springmvccrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvccrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
